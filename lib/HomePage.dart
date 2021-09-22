ef593883c577c2b31c1302bebdf5fbda1ade90be
Ranaco <ranasatyamraj@gmail.com>
2021-09-22 23:17:28 +0530

    all done

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,36 +1,36 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
 class HomePage extends StatefulWidget {
-  final String? email, password, title;
-  const HomePage({Key? key, this.title, this.email, this.password})
-      : super(key: key);
+  final String? title;
+  const HomePage({
+    Key? key,
+    this.title,
+  }) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var data = [];
+
   String url = "https://api.github.com/users";
 
-  List data = [];
   bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    this.getUserData();
+    getUserData();
   }
 
   Future getUserData() async {
-    var response = await http.get(
-      Uri.parse(url),
-    );
-    // print(response.body);
+    var response = await http.get(Uri.parse(url));
     setState(() {
       data = jsonDecode(response.body);
       print(data);
@@ -45,30 +45,22 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text(
-                    data[index]["login"],
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  subtitle: Text(
-                    data[index]["url"],
-                    style: TextStyle(fontSize: 15.0),
-                  ),
-                )
-              ],
-            ),
-          );
-        },
-      ),
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, index) {
+            return ListTile(
+              leading: Icon(Icons.person),
+              title: Text(data[index]["login"],
+                  style: TextStyle(
+                    fontSize: 20,
+                  )),
+              subtitle: Text(
+                data[index]["url"],
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            );
+          }),
     );
   }
 }
